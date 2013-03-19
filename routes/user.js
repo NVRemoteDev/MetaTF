0b5811b9ce8fa8e927f16078d566e9962f551ac2
Noppadet <tonygithub@outlook.com>
2013-03-18 20:00:55 -0700

    Add header footer navigation template includes

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,15 @@
-/*
+/**
+ * Module Dependencies
+ */
+
+/**
  * GET /backpack/:id (:id being 64bit steam id)
  */
 exports.backpack = function(req, res, next) {
   var getBackpack = require('../controllers/backpack');
   getBackpack(req.params.id, function (err, backpackitems, backpackSlots) {
     if (err) return next(err);
-    res.render('backpack', { results: backpackitems, user: req.params.id, bpslots: backpackSlots });
+    res.render('backpack', { title: 'backpack', results: backpackitems, user: req.params.id, bpslots: backpackSlots });
   });
 };
 
@@ -13,14 +17,6 @@ exports.schema = function(req, res, next) {
   var getSchema = require('../controllers/importSchema');
   getSchema(function (err, schema) {
     if (err) return next(err);
-    res.render('schema', { results: schema });
-  });
-};
-
-exports.openid = function(req, res, next) {
-  var login = require('../controllers/authentication');
-  getSchema(function (err, schema) {
-    if (err) return next(err);
-    res.render('schema', { results: schema });
+    res.render('schema', { title: 'schema', user: req.user, results: schema });
   });
 };
