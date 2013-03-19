45bd30174cc0977cd45f6f9a85f2527c96396b08
Noppadet <tonygithub@outlook.com>
2013-03-18 20:12:11 -0700

    Change 'user' var in backpack to id to work correctly with login system

--- routes/user.js
+++ routes/user.js
@@ -9,7 +9,7 @@ exports.backpack = function(req, res, next) {
   var getBackpack = require('../controllers/backpack');
   getBackpack(req.params.id, function (err, backpackitems, backpackSlots) {
     if (err) return next(err);
-    res.render('backpack', { title: 'backpack', results: backpackitems, user: req.params.id, bpslots: backpackSlots });
+    res.render('backpack', { title: 'backpack', results: backpackitems, id: req.params.id, user: req.user, bpslots: backpackSlots });
   });
 };
 
