de4ca4f61febab6323225eeb1d4639e0e58d0b02
Noppadet <tonygithub@outlook.com>
2013-03-23 11:49:36 -0700

    Change session expiration to 2 weeks

--- server.js
+++ server.js
@@ -88,11 +88,11 @@ app.configure(function(){
     secret: 'dont be walmarting',
     store: new MongoStore(sess_conf.db),
     cookie: {
-      maxAge: new Date(Date.now() + 1209600),
-      expires: new Date(Date.now() + 1209600)
+      maxAge: new Date(Date.now() + 12096000),
+      expires: new Date(Date.now() + 12096000)
     },
-    maxAge : new Date(Date.now() + 1209600),
-    expires: new Date(Date.now() + 1209600)
+    maxAge : new Date(Date.now() + 12096000),
+    expires: new Date(Date.now() + 12096000)
   }));
   // Initialize Passport!  Also use passport.session() middleware, to support
   // persistent login sessions (recommended).
