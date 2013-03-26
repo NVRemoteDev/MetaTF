bba686c33ccbe321d180d2002a4589ec728cd320
Noppadet <tonygithub@outlook.com>
2013-03-25 23:17:45 -0700

    Fix erroneous user data from new user on homepage

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,12 @@
  */
 
 exports.index = function(req, res){
-  if(req.user) var steamID = req.user.steamid; // If user is logged in, set req.user to their steam id
-  require('../controllers/user_controller').get(steamID, function(err, doc) {
-    if (err) throw err;
-    if(!doc) {
-      res.render('index', { title: 'Meta.tf', user: null });
-    } else {
+  if(req.user) {
+    var steamID = req.user.steamid; // If user is logged in, set req.user to their steam id
+    require('../controllers/user_controller').get(steamID, function(err, doc) {
+      if (err) throw err;
       res.render('index', { title: 'Meta.tf', user: doc });
-    }
-  });
+    });
+  }
+  res.render('index', { title: 'Meta.tf', user: null });
 };
