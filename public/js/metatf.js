6c8cf9b6198137e273a5abba9c8d83496aeb67fb
Noppadet <tonygithub@outlook.com>
2013-04-06 19:10:00 -0700

    Add more information to tooltips and backpack viewer

--- public/js/metatf.js
+++ public/js/metatf.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
   // Handler for .ready() called.
 
   $(".bpitem").mousemove(function (e) {
-    $("div", this).addClass('show').css('left', e.pageX - 70).css('top', e.pageY - 110);
+    $(".iteminfo", this).addClass('show').css('left', e.pageX - 70).css('top', e.pageY - 110);
   });
 
   $(".bpitem").mouseout(function () {
-    $("div", this).removeClass('show');
+    $(".iteminfo", this).removeClass('show');
   });
 });
