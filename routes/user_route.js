f8846195c32d3df961b24dccd402b6f71aa8d21e
Noppadet <tonygithub@outlook.com>
2013-04-10 14:01:51 -0700

    Add some error catches

--- routes/user_route.js
+++ routes/user_route.js
@@ -212,24 +212,32 @@ exports.showbackpack = function(req, res, next) {
     // Add the backpack checkee to db if necessary
     if(req.user) {
       require('../controllers/user_controller').get(req.user.steamid, function(err, doc) {
-      if (err) throw err;
-        require('../models/user_models').checkIfUserAddToDbIfNot(req.params.id); // Add owner of checked backpack to database if necessary
-        require('../models/user_models').pullUserDataFromSteamAPI(req.params.id); // Get the user's information, add it to database
-        require('../controllers/user_controller').get(req.params.id, function(err, backpackOwner) {
           if (err) throw err;
-          res.render('backpack', { title: 'Backpack', items: bpitems,
-            id: req.params.id, bpslots: backpackslots, user: doc, bpowner: backpackOwner, newItems: backpackHasNewIems });
+          require('../models/user_models').checkIfUserAddToDbIfNot(req.params.id); // Add owner of checked backpack to database if necessary
+          require('../models/user_models').pullUserDataFromSteamAPI(req.params.id); // Get the user's information, add it to database
+          require('../controllers/user_controller').get(req.params.id, function(err, backpackOwner) {
+            if(doc && backpackOwner) {
+              if (err) throw err;
+              res.render('backpack', { title: 'Backpack', items: bpitems,
+                id: req.params.id, bpslots: backpackslots, user: doc, bpowner: backpackOwner, newItems: backpackHasNewIems });
+            } else {
+              res.render('/');
+            }
           });
-        });
-      } else { // Not a logged in user; no navbar params
-        require('../models/user_models').checkIfUserAddToDbIfNot(req.params.id);
-        require('../models/user_models').pullUserDataFromSteamAPI(req.params.id);
-        require('../controllers/user_controller').get(req.params.id, function(err, backpackOwner) {
+      });
+    } else { // Not a logged in user; no navbar params
+      require('../models/user_models').checkIfUserAddToDbIfNot(req.params.id);
+      require('../models/user_models').pullUserDataFromSteamAPI(req.params.id);
+      require('../controllers/user_controller').get(req.params.id, function(err, backpackOwner) {
+        if(backpackOwner) {
           if (err) throw err;
           res.render('backpack', { title: 'Backpack', items: bpitems,
             id: req.params.id, bpslots: backpackslots, user: null, bpowner: backpackOwner, newItems: backpackHasNewIems });
-        });
-      }
+        } else {
+          res.render('/');
+        }
+      });
+    }
   });
 };
 
