1c0af8c2ac166f93aefd83c0b64ee592e28fa114
Noppadet <tonygithub@outlook.com>
2013-04-14 12:10:02 -0700

    Change render in item_controller.js to better follow DRY principals

--- controllers/item_controller.js
+++ controllers/item_controller.js
@@ -12,51 +12,37 @@ exports.showbackpack = function (req, res, next) {
     var bpitems = backpackitems.items;
     var backpackslots = backpackitems.num_backpack_slots;
 
-    // User is logged in
-    // Send the params for the navbar
-    // Add the backpack checkee to db if necessary
-    if (req.user) {
-      require('../models/user_model').get(req.user.steamid, function (err, doc) {
-        if (err) throw err;
-        require('../models/user_model').checkIfUserAddToDbIfNot(req.params.id); // Add owner of checked backpack to database if necessary
-        require('../models/user_model').pullUserDataFromSteamAPI(req.params.id); // Get the user's information, add it to database
-        require('../models/user_model').get(req.params.id, function (err, backpackOwner) {
-          if (doc && backpackOwner) {
-            if (err) throw err;
-            res.render('backpack', {
-              title: 'Backpack',
-              items: bpitems,
-              id: req.params.id,
-              bpslots: backpackslots,
-              user: doc,
-              bpowner: backpackOwner,
-              newItems: backpackHasNewIems
-            });
-          } else {
-            res.render('/');
-          }
-        });
-      });
-    } else { // Not a logged in user; no navbar params
-      require('../models/user_model').checkIfUserAddToDbIfNot(req.params.id);
-      require('../models/user_model').pullUserDataFromSteamAPI(req.params.id);
+    // Add the backpack owner to db if necessary
+    var steamID = '';
+    if (req.user) steamID = req.user.steamid;
+    require('../models/user_model').get(steamID, function (err, doc) {
+      if (err) throw err;
+      require('../models/user_model').checkIfUserAddToDbIfNot(req.params.id); // Add owner of checked backpack to database if necessary
+      require('../models/user_model').pullUserDataFromSteamAPI(req.params.id); // Get the user's information, add it to database
       require('../models/user_model').get(req.params.id, function (err, backpackOwner) {
-        if (backpackOwner) {
-          if (err) throw err;
+        if (doc) {
           res.render('backpack', {
-            title: 'Backpack',
-            items: bpitems,
-            id: req.params.id,
-            bpslots: backpackslots,
-            user: null,
-            bpowner: backpackOwner,
-            newItems: backpackHasNewIems
-          });
+                title: 'Backpack',
+                items: bpitems,
+                id: req.params.id,
+                bpslots: backpackslots,
+                user: doc,
+                bpowner: backpackOwner,
+                newItems: backpackHasNewIems
+              });
         } else {
-          res.render('/');
+          res.render('backpack', {
+                title: 'Backpack',
+                items: bpitems,
+                id: req.params.id,
+                bpslots: backpackslots,
+                user: null,
+                bpowner: backpackOwner,
+                newItems: backpackHasNewIems
+              });
         }
       });
-    }
+    });
   });
 };
 
