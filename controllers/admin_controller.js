0548a6e056e23f6af830fc8de739886b3bfe5719
Noppadet <tonygithub@outlook.com>
2013-04-14 10:57:40 -0700

    Finish refactoring admin_controller to point to correct model files

--- controllers/admin_controller.js
+++ controllers/admin_controller.js
@@ -7,16 +7,16 @@ exports.index = function(req, res, next) {
   var action = req.params.action;
   var steamID = req.user.steamid;
   var message = null;
-  
+
   if(action === 'updateSchema') {
-    require('./controllers/download_schema_controller').download(req, res, next); // Downloads and writes schema
+    require('../models/download_schema_model').download(req, res, next); // Downloads and writes schema
     message = 'Downloading and writing schema...';
     require('../models/user_model').get(steamID, function(err, doc) {
       if (err) throw err;
         res.render('admin', { title: 'Admin Area', user: doc, message: message });
     });
   } else if(action === 'resizeImages') {
-    require('./controllers/download_schema_controller').resizeitems(req, res, next); // Downloads and writes schema
+    require('../models/download_schema_model').resizeitems(req, res, next); // Downloads and writes schema
     message = 'Resizing images...';
     require('../controllers/user_controller').get(steamID, function(err, doc) {
       if (err) throw err;
