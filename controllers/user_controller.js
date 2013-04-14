ff818855546e74d08d4a0a33e0dc297056a21ced
Noppadet <tonygithub@outlook.com>
2013-04-14 15:05:22 -0700

    user_controller.js: Use better DRY principals, add better error protection. Clarify comments

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -2,37 +2,40 @@
  * Module dependencies.
  */
 
- var mongoose = require('mongoose')
-   , Users = mongoose.model("Users");
+ var mongoose = require('mongoose'),
+     Users = mongoose.model("Users");
 
+/**
+ * Renders the profile page which contains their trades
+ */
 exports.profile = function (req, res, next) {
   var PullFromSteamApi = require('../models/steamapi_model');
-  if (req.user !== undefined) {
-    require('../models/user_model').get(req.user.steamid, function (err, doc) {
-      if (err) throw err;
-      require('../models/user_model').checkIfUserAddToDbIfNot(req.params.id); // Add owner of checked backpack to database if necessary
-      require('../models/user_model').pullUserDataFromSteamAPI(req.params.id); // Get the user's information, add it to database
-      require('../models/user_model').get(req.params.id, function (err, profileowner) {
-        if (err) throw err;
-        res.render('profile', {
-          title: 'Profile',
-          id: req.params.id,
-          user: doc,
-          profileowner: profileowner
-        });
-      });
-    });
-  } else { // Not a logged in user; no navbar params
+  // Not a logged in user; no navbar params
+  var steamID = '';
+  if (req.user) steamID = req.user.steamid;
+  require('../models/user_model').get(steamID, function (err, loggedin) {
+    if (err) throw err;
     require('../models/user_model').checkIfUserAddToDbIfNot(req.params.id);
     require('../models/user_model').pullUserDataFromSteamAPI(req.params.id);
     require('../models/user_model').get(req.params.id, function (err, doc) {
       if (err) throw err;
-      res.render('profile', {
-        title: 'Profile',
-        id: req.params.id,
-        user: null,
-        profileowner: doc
-      });
+      if(loggedin && doc) { // user is logged in, profile we're checking exists
+        res.render('profile', {
+          title: 'Profile',
+          id: req.params.id,
+          user: loggedin,
+          profileowner: doc
+        });
+      } else if(doc) { // user isn't logged in, profile we're checking exists
+        res.render('profile', {
+          title: 'Profile',
+          id: req.params.id,
+          user: null,
+          profileowner: doc
+        });
+      } else { // error
+        res.redirect('/');
+      }
     });
-  }
+  });
 };
